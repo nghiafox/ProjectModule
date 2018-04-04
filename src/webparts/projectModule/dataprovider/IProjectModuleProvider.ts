355d8e371eddba2b6d35d2bb6214c7c26e8513ab
Tin Trung Nguyen <tintrungnguyen87@gmail.com>
2018-04-04 15:13:42 +0700

    using react-treebeard to build treeview and backend using sp-pnp-js.

---
+++ src/webparts/projectModule/dataprovider/IProjectModuleProvider.ts
@@ -0,0 +1,11 @@
+import { IWebPartContext } from '@microsoft/sp-webpart-base';
+
+interface IProjectModuleProvider {
+    webPartContext: IWebPartContext;
+    getFoldersDocumentSetByListName(listName:string);
+    getFoldersDocumentSetByRootFolder(rootFolder: string);
+    getRootFolderById(listName:string,itemid:number);
+    getItemsByListName(listName: string, columns:string);
+}
+
+export default IProjectModuleProvider;
